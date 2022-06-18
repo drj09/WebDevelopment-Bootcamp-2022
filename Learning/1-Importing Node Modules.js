f539a5cd04e625a30a9f5c665ce3cdb4cd0932ac
Dheeraj <dheeraj160199@gmail.com>
2022-06-18 23:46:45 +0530

    importing node modules

---
+++ Learning/1-Importing Node Modules.js
@@ -0,0 +1,25 @@
+// Using Native Node Modules
+    
+    const fs = require("fs");
+    function copyFiles() {
+        // using function to copy file1 to file2
+        console.log("Copy File");
+        var a = fs.copyFileSync("file1.txt","file2.txt");
+    }
+
+
+// using external Node Modules : npm install superheroes
+    var superheroes = require('superheroes');
+    function superHeroNames() {
+        let name =superheroes.random();
+        console.log(name);
+    }
+
+
+
+
+// function to run 
+    copyFiles();
+    superHeroNames();
+
+
