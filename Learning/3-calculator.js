8867704cc7f1b96917fef7ad8f8ce01fdebc9f17
Dheeraj <dheeraj160199@gmail.com>
2022-06-19 23:44:59 +0530

    Created a calculator

--- Learning/3-calculator.js
+++ Learning/3-calculator.js
@@ -1,6 +1,21 @@
 const express = require("express");
-const app = express();
+const bodyParser = require("body-parser");
 
-app.get("/",function(req,res) {
-    res.send("Hello World");
-})
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/3-calculator.html");
+});
+
+app.post("/", function (req, res) {
+    let num1 = Number(req.body.n1);
+    let num2 = Number(req.body.n2);
+
+    var result = num1+num2;
+    res.send( "the result is : " + result);
+});
+
+app.listen(3000, function () {
+  console.log("Server is Up and Running on 3000");
+});
