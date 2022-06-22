0ff60ff71156ca206bb1e044905a3ed1d1cf70ba
Dheeraj <dheeraj160199@gmail.com>
2022-06-22 23:26:09 +0530

    passing data from frontend to backend

--- Learning/5-templating.js
+++ Learning/5-templating.js
@@ -1,16 +1,29 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 
 app.set("view engine", "ejs");
+
+var item = [];
+
+app.use(bodyParser.urlencoded({extended:true}));
 
 app.get("/", function (req, res) {
   var today = new Date();
   var day = "";
   if (today.getDay() == 6 || today.getDay() == 0) day = "Weekend";
   else day = "Work Day";
+  
 
-  res.render("list", { kindOfDay: day });
+  res.render("list", { kindOfDay: day,newListItem : item });
 });
+
+app.post("/",function (req,res) {
+    item.push(req.body.newItem);
+    console.log(item);
+
+    res.redirect("/");
+})
 
 app.listen(3000, function () {
   console.log("server is up to run on port 3000");
