060cda9f52d59a2882d346e677abe20e0f033cd6
Dheeraj <dheeraj160199@gmail.com>
2022-06-26 19:46:19 +0530

    added gitignore file

--- Learning/6-apiCreation.js
+++ Learning/6-apiCreation.js
@@ -24,6 +24,7 @@ const articleSchema = {
 
 const Article = mongoose.model("Article", articleSchema);
 
+// Request REST API Targeting all data
 app
   .route("/articles")
   .get(function (res, res) {
@@ -53,6 +54,65 @@ app
       else res.send(err);
     });
   });
+
+
+// Request REST API Targeting specific data
+app.route("/articles/:articleTitle")
+
+.get(function(req, res){
+
+  Article.findOne({title: req.params.articleTitle}, function(err, foundArticle){
+    if (foundArticle) {
+      res.send(foundArticle);
+    } else {
+      res.send("No articles matching that title was found.");
+    }
+  });
+})
+
+.put(function(req, res){
+
+  Article.update(
+    {title: req.params.articleTitle},
+    {title: req.body.title, content: req.body.content},
+    {overwrite: true},
+    function(err){
+      if(!err){
+        res.send("Successfully updated the selected article.");
+      }
+    }
+  );
+})
+
+.patch(function(req, res){
+
+  Article.update(
+    {title: req.params.articleTitle},
+    {$set: req.body},
+    function(err){
+      if(!err){
+        res.send("Successfully updated article.");
+      } else {
+        res.send(err);
+      }
+    }
+  );
+})
+
+.delete(function(req, res){
+
+  Article.deleteOne(
+    {title: req.params.articleTitle},
+    function(err){
+      if (!err){
+        res.send("Successfully deleted the corresponding article.");
+      } else {
+        res.send(err);
+      }
+    }
+  );
+});
+
 
 app.listen(3000, function () {
   console.log("Server started on port 3000");
