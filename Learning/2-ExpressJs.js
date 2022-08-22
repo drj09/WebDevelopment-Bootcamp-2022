0fa7727d06b155c3a26a1321420a2d51644ba9c5
Dheeraj <dheeraj160199@gmail.com>
2022-08-22 11:43:38 +0530

    empty commit

--- Learning/2-ExpressJs.js
+++ Learning/2-ExpressJs.js
@@ -1,21 +1,16 @@
 const express = require("express");
 const app = express();
 
-app.get("/",function(request,response){
-    console.log('got new request on homePage');
-    response.send("<h1>Home Page</h1>");
-}
-)
-
-app.get("/contact",function(request,response){
-        console.log('got new on contactPage');
-        response.send("<h1>Contact Page</h1>");
-    }
-)
-
-app.listen(3000,function(){
-    console.log("server started on port 3000");
+app.get("/", function (request, response) {
+  console.log("got new request on homePage");
+  response.send("<h1>Home Page2</h1>");
 });
 
+app.get("/contact", function (request, response) {
+  console.log("got new on contactPage");
+  response.send("<h1>Contact Page</h1>");
+});
 
-
+app.listen(3000, function () {
+  console.log("server started on port 3000");
+});
